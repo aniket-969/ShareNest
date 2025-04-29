2857676b57cdb1abb42c9391342dc7a7756f5108
Aniket <aniketbaranwal8090@gmail.com>
2025-04-29 23:31:28 +0530

    Modifying zod validation for poll schema

--- server/src/zod/poll.schema.js
+++ server/src/zod/poll.schema.js
@@ -4,12 +4,16 @@ import { z } from "zod";
 export const pollSchema = z.object({
   title: stringValidation(1, 100, "title"),
   status: z.enum(["active", "completed", "closed"]).optional(),
-  voteEndTime: z
-    .string()
-    .transform((val) => new Date(val))
-    .refine((date) => !isNaN(date.getTime()), {
-      message: "Invalid date format",
-    }), 
+  voteEndTime: z.coerce.date().refine(
+    (date) => {
+      const now = new Date();
+      const maxAllowedDate = new Date(now.getTime() + 7 * 24 * 60 * 60 * 1000); 
+      return date > now && date <= maxAllowedDate;
+    },
+    {
+      message: "Vote end time must be within 7 days from now.",
+    }
+  ),
   options: z
     .array(stringValidation(1, 100, "Option Text"))
     .min(1, { message: "At least one option is required" }),
