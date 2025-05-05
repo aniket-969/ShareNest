e553269d7a1201b6793d6f9a9b1b916f39b506eb
Aniket <aniketbaranwal8090@gmail.com>
2025-05-06 00:30:31 +0530

    Added loginLimiter and sessionLimiter

---
+++ server/src/middleware/rateLimiters.js
@@ -0,0 +1,32 @@
+import rateLimit from "express-rate-limit";
+import { ApiError } from "../utils/ApiError.js";
+
+// Limit login attempts per IP
+export const loginLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 5,
+  standardHeaders: true,
+  legacyHeaders: false,
+  handler: (_, __, ___, options) => {
+    throw new ApiError(
+      options.statusCode || 429,
+      `Too many login attempts. Try again after ${
+        options.windowMs / 1000
+      } seconds.`
+    );
+  },
+});
+
+// Limit session validation (called automatically on page load)
+export const sessionLimiter = rateLimit({
+  windowMs: 30 * 1000,
+  max: 5,
+  standardHeaders: true,
+  legacyHeaders: false,
+  handler: (_, __, ___, options) => {
+    throw new ApiError(
+      options.statusCode || 429,
+      `Too many session checks. Slow down.`
+    );
+  },
+});
