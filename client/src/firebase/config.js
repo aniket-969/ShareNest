1ca6601e3c60c1921c01670273cb2ffa7820fc1b
Aniket <aniketbaranwal8090@gmail.com>
2025-05-29 18:28:34 +0530

    Added firebase configurations

---
+++ client/src/firebase/config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAhpGKtjseB2Cpp_o48FbiIpoLLX_lZuTY",
+  authDomain: "sharenest-eac0e.firebaseapp.com",
+  projectId: "sharenest-eac0e",
+  storageBucket: "sharenest-eac0e.firebasestorage.app",
+  messagingSenderId: "698125212339",
+  appId: "1:698125212339:web:8ae2f7fd37f3861146aec6",
+  measurementId: "G-635LBE9VEN"
+};
+
+const app = initializeApp(firebaseConfig);
