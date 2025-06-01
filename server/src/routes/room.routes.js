61aad02f5f72e4807bad5682665e4ebc57ca1a64
Aniket <aniketbaranwal8090@gmail.com>
2025-06-01 21:27:10 +0530

    Updating routes for updatedController

--- server/src/routes/room.routes.js
+++ server/src/routes/room.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { addUserRequest, adminResponse, createRoom, deleteRoom, getRoomData, leaveRoom, transferAdminControl, updateRoom } from "../controllers/room.controller.js";
 import { verifyJWT } from './../middleware/auth.middleware.js';
 import { adminOnly, checkMember } from "../middleware/room.middleware.js";
-import { addUserRequestSchema, adminResponseSchema, creatRoomSchema, transferRoleSchema } from "../zod/room.schema.js";
+import { addUserRequestSchema, adminResponseSchema, creatRoomSchema, transferRoleSchema, updateRoomSchema } from "../zod/room.schema.js";
 import { validate } from './../middleware/validator.middleware.js';
  
 const router = Router();
@@ -13,7 +13,7 @@ router.route("/:roomId/responses").post(verifyJWT,validate(adminResponseSchema),
 router.route("/:roomId").patch(verifyJWT,updateRoom);
 router.route("/:roomId").delete(verifyJWT,deleteRoom);
 router.route("/:roomId").get(verifyJWT,checkMember,getRoomData);
-router.route("/:roomId/leave").patch(verifyJWT,checkMember,leaveRoom);
+router.route("/:roomId/leave").patch(verifyJWT,checkMember,validate(updateRoomSchema),leaveRoom);
 router.route("/:roomId/admin/:newAdminId").patch(verifyJWT,adminOnly,transferAdminControl);
 
 export default router;
