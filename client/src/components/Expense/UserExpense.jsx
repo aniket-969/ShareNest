aebc09ab7fa77086b9747acc503ffaec41d263ad
Aniket <aniketbaranwal8090@gmail.com>
2025-06-03 17:48:05 +0530

    Fixed carousel issue for expense card

--- client/src/components/Expense/UserExpense.jsx
+++ client/src/components/Expense/UserExpense.jsx
@@ -4,7 +4,13 @@ import { useParams } from "react-router-dom";
 import { Spinner } from "./../ui/spinner";
 import ExpenseCard from "./ExpenseCard";
 import { ScrollArea } from "../ui/scroll-area";
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "../ui/carousel";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../ui/carousel";
 import { Card, CardContent } from "../ui/card";
 
 const UserExpense = ({ userExpenseQuery, userId }) => {
@@ -17,33 +23,32 @@ const UserExpense = ({ userExpenseQuery, userId }) => {
   }
   console.log("Userx", expenses[0]);
   return (
-    <div>
+    <div className="m-2">
       <h3>To Pay</h3>
-      
-         <Carousel
-      opts={{
-        align: "start",
-      }}
-      className="w-[40rem]"
-    >
-      <CarouselContent>
-        {expenses.map((expense) => (
-          <CarouselItem key={expense._id} className="md:basis-1/2 lg:basis-1/3">
-            <div className="p-1">
-            
-                  <ExpenseCard key={expense._id} userId={userId} expense={expense} />
-                
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
-       
-     
+
+      <Carousel className=" lg:w-[56rem] md:w-[38rem] w-[20rem] my-4">
+        <CarouselContent className="-ml-4">
+          {expenses.map((expense) => (
+            <CarouselItem
+              key={expense._id}
+              className="pl-4 md:basis-1/2 lg:basis-1/3"
+            >
+                <div className="p-1 ">
+                    <ExpenseCard
+                key={expense._id}
+                userId={userId}
+                expense={expense}
+              /> 
+                </div>
+             
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </div>
   );
 };
-//  <ExpenseCard key={expense._id} userId={userId} expense={expense} />
+
 export default UserExpense;
