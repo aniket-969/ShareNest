6db414b60324cb1eb002fceac3e664d419837729
Aniket <aniketbaranwal8090@gmail.com>
2026-01-18 22:20:47 +0530

    Frontend oauth google integratio complete

--- client/src/components/Auth/googleLogin.jsx
+++ client/src/components/Auth/googleLogin.jsx
@@ -2,46 +2,21 @@ import React from "react";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { Button } from "@/components/ui/button";
 import { useAuth } from "@/hooks/useAuth";
-import { GoogleLogin } from '@react-oauth/google';
+import { GoogleLogin } from "@react-oauth/google";
 
- 
 const LoginGoogle = () => {
-  const provider = new GoogleAuthProvider();
-  const auth = getAuth();
-const {loginWithGoogleMutation} = useAuth()
-  const handleGoogleLogin = async () => {
-    try {
-      const result = await signInWithPopup(auth, provider);
-    //   const credential = GoogleAuthProvider.credentialFromResult(result);
-    //   console.log(credential)
-    //   const token = credential.accessToken;
-    //   const user = result.user;
-    //   console.log("Google login success:", user, token);
-     const user = result.user;
-
-    // Get the Firebase ID token (this is what your backend expects)
-    const idToken = await user.getIdToken();
-
-    // Call your mutation with the idToken
-    loginWithGoogleMutation.mutate(idToken);
-
-    console.log("Google login success:", user.email, idToken);
-    } catch (error) {
-      console.error("Google login error:", error);
-    }
-  };
+  const { loginWithGoogleMutation } = useAuth();
 
   return (
-   
-      <GoogleLogin
-  onSuccess={credentialResponse => {
-    console.log(credentialResponse);
-  }}
-  onError={() => {
-    console.log('Login Failed');
-  }}
-/>
+    <GoogleLogin
+      onSuccess={(credentialResponse) => {
+        const idToken = credentialResponse.credential;
+        loginWithGoogleMutation.mutate(idToken);
+      }}
+      onError={() => {
+        console.log("Google Login Failed");
+      }}
+    />
   );
 };
-
 export default LoginGoogle;
