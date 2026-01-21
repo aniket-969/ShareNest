dc7ccc7ebfd30f549e202bbb2cf85bf72110b6ce
Aniket <aniketbaranwal8090@gmail.com>
2026-01-21 23:23:26 +0530

    Added indicator for online members with truncate for name

--- client/src/components/Sidebar/RoomMembers.jsx
+++ client/src/components/Sidebar/RoomMembers.jsx
@@ -3,8 +3,15 @@ import { Users } from "lucide-react";
 import { Badge } from "../ui/badge";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 
-const RoomMembers = ({ tenants, showMembers, toggleMembers, admin, onlineMembers }) => {
-  console.log(onlineMembers)
+const RoomMembers = ({
+  tenants,
+  showMembers,
+  toggleMembers,
+  admin,
+  onlineMembers,
+}) => {
+  const onlineMembersSet = new Set(onlineMembers);
+  console.log(onlineMembersSet,onlineMembers);
   return (
     <div className="p-4 border-b">
       {/* Header row */}
@@ -13,8 +20,12 @@ const RoomMembers = ({ tenants, showMembers, toggleMembers, admin, onlineMembers
         onClick={toggleMembers}
       >
         <Users className="w-5 h-5" />
-        <span className="flex-1 font-medium">Members [ <span className="h-[0.6rem] w-[0.6rem] rounded-full bg-green-400 inline-block" />  {onlineMembers.length} ]</span>
-        
+        <span className="flex-1 font-medium">
+          Members [{" "}
+          <span className="h-[0.6rem] w-[0.6rem] rounded-full bg-green-400 inline-block" />{" "}
+          {onlineMembers.length} ]
+        </span>
+
         <span className="text-xs bg-secondary px-2 text-black font-semibold py-1 rounded-full">
           {tenants?.length || 0}
         </span>
@@ -27,33 +38,39 @@ const RoomMembers = ({ tenants, showMembers, toggleMembers, admin, onlineMembers
             <div className="space-y-2 pr-3">
               {tenants.map((member) => (
                 <div
-                  key={member._id}
+                  key={member?._id}
                   className="flex items-center p-2 bg-secondary/20 rounded-lg"
                 >
-                  <Avatar className="w-8 h-8 mr-3 border">
-                    <AvatarImage src={member.avatar} alt={member.fullName} />
-                    <AvatarFallback>
-                      {/* You can use initials or an alt image here */}
-                      {(
-                        <img
-                          src="/altAvatar1.jpg"
-                          alt="fallback avatar"
-                          
-                        />
-                      ) || member.fullName?.charAt(0)}
-                    </AvatarFallback>
-                  </Avatar>
+                  {/* avatar and name */}
+                  <div className="flex items-center flex-1">
+                    <Avatar className="w-8 h-8 mr-3 border">
+                      <AvatarImage
+                        src={member?.avatar}
+                        alt={member?.fullName?.charAt(0)}
+                      />
+                      <AvatarFallback>
+                        {/* You can use initials or an alt image here */}
+                        {<img src="/altAvatar1.jpg" alt="fallback avatar" /> ||
+                          member?.fullName?.charAt(0)}
+                      </AvatarFallback>
+                    </Avatar>
+                    <span className="text-sm font-medium max-w-[90px] truncate">
+                      {member?.fullName}
+                    </span>
+                  </div>
 
-                  <span className="text-sm font-medium flex items-center gap-8">
-                    {member.fullName}
-                    {member._id === admin._id && (
-                      <Badge className="text-[0.65rem] bg-secondary/20 px-1.5 rounded-sm hover:bg-secondary/20">
+                  {/* admin badge */}
+                  <span className="text-sm font-medium">
+                    {member?._id === admin._id && (
+                      <Badge className="text-[0.65rem] bg-secondary/20 px-1.5 rounded-sm hover:bg-secondary/20 mr-5">
                         Admin
                       </Badge>
                     )}
-                   
                   </span>
-                  
+                  {/* online green indicator */}
+                  {onlineMembersSet.has(member?._id) && (
+                    <span className="h-[0.6rem] w-[0.6rem] rounded-full bg-green-400 inline-block" />
+                  )}
                 </div>
               ))}
             </div>
