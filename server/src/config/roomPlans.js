653ffd1818ba134467472e5e11f06cd8dad22ccc
Aniket <aniketbaranwal8090@gmail.com>
2026-02-04 19:44:12 +0530

    Added roomconfig for pricing plans

---
+++ server/src/config/roomPlans.js
@@ -0,0 +1,63 @@
+export const ROOM_PLANS = {
+  IN: {
+    region: "IN",
+    currency: "INR",
+    plans: {
+      free: {
+        planId: "free",
+        label: "Free",
+        price: 0,
+        paid: false,
+        billingCycle: null,
+        maxMembers: 1,
+      },
+      pro_monthly: {
+        planId: "pro_monthly",
+        label: "Monthly",
+        price: 99,
+        paid: true,
+        billingCycle: "monthly",
+        maxMembers: 6,
+      },
+      pro_yearly: {
+        planId: "pro_yearly",
+        label: "Yearly",
+        price: 999,
+        paid: true,
+        billingCycle: "yearly",
+        maxMembers: 6,
+      },
+    },
+  },
+
+  USD: {
+    region: "USD",
+    currency: "USD",
+    plans: {
+      free: {
+        planId: "free",
+        label: "Free",
+        price: 0,
+        paid: false,
+        billingCycle: null,
+        maxMembers: 1,
+      },
+      pro_monthly: {
+        planId: "pro_monthly",
+        label: "Monthly",
+        price: 2.49,
+        paid: true,
+        billingCycle: "monthly",
+        maxMembers: 6,
+      },
+      pro_yearly: {
+        planId: "pro_yearly",
+        label: "Yearly",
+        price: 24.99,
+        paid: true,
+        billingCycle: "yearly",
+        maxMembers: 6,
+      },
+    },
+  },
+};
