c74184ab2434375b00c9890fb645f9090c2f5462
Aniket <aniketbaranwal8090@gmail.com>
2026-02-06 08:35:53 +0530

    Integrated roomPaymentDetails api to roomPayments

--- client/src/components/Room/roomPayment.jsx
+++ client/src/components/Room/roomPayment.jsx
@@ -1,6 +1,14 @@
+import { useRoom } from '@/hooks/useRoom';
 import React from 'react'
+import { useParams } from 'react-router-dom';
 
 const RoomPayment = () => {
+
+    const {roomId} = useParams()
+    const {roomPaymentDetails} = useRoom(roomId)
+    const {data,isLoading, isError} = roomPaymentDetails
+    console.log(data)
+
   return (
     <div>RoomPayment</div>
   )
