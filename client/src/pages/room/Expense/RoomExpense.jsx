c11b7880c78e162c57aad865f2f0417022ce8635
Aniket <aniketbaranwal8090@gmail.com>
2026-03-13 21:38:21 +0530

    Added scrollToPayments icon with functionality

--- client/src/pages/room/Expense/RoomExpense.jsx
+++ client/src/pages/room/Expense/RoomExpense.jsx
@@ -1,82 +1,83 @@
-import { Suspense, useState, lazy, useMemo } from "react";
+import { Suspense, useState, lazy, useMemo, useRef } from "react";
 import { Spinner } from "@/components/ui/spinner";
 import { useExpense } from "@/hooks/useExpense";
 import { useRoom } from "@/hooks/useRoom";
 import { useParams } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import BalanceSheet from "@/components/Expense/balanceSheet";
-import { Search, CirclePlus, ScrollText } from "lucide-react";
+import {
+  Search,
+  CirclePlus,
+  ScrollText,
+  BanknoteArrowDown,
+} from "lucide-react";
 import ExpenseContainer from "@/components/Expense/ExpenseContainer";
 import { Card } from "@/components/ui/card";
 import SearchOverlay from "@/components/Expense/searchOverlay";
 
 const ExpenseForm = lazy(() => import("@/components/form/ExpenseForm"));
 const FormWrapper = lazy(() => import("@/components/ui/formWrapper"));
-const PaymentDetails = lazy(()=> import("@/components/Expense/paymentDetails"))
+const PaymentDetails = lazy(
+  () => import("@/components/Expense/paymentDetails")
+);
 
 const RoomExpense = () => {
   const { roomId } = useParams();
   const { createExpenseMutation } = useExpense(roomId);
   const { roomQuery } = useRoom(roomId);
-  const { data, isLoading, isError } = roomQuery;
+  const { data } = roomQuery;
+  const paymentRef = useRef(null);
   const [isFormOpen, setIsFormOpen] = useState(false);
   const [isSearchOpen, setIsSearchOpen] = useState(false);
+
+  const scrollToPayments = () => {
+    paymentRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+ 
   const userData = localStorage.getItem("session");
   const userId = JSON.parse(userData)?._id;
 
   const participants = [...(data?.tenants || [])];
 
   return (
-    <div className="">
-      <div className="flex flex-col gap-6 items-center ">
-      {/* Heading and icons*/}
-      <div className="flex items-center justify-around w-full ">
-        <h2 className="font-bold text-2xl">Expense</h2>
-        {/* icons */}
-        <div className="flex gap-3">
-          <Button
-            className="md:hidden"
-            size="icon"
-            variant="primary"
-            onClick={() => setIsFormOpen(true)}
-          >
-            <CirclePlus />
-          </Button>
-          <Button
-            size="icon"
-            variant="outline"
-            onClick={() => setIsSearchOpen(true)}
-          >
-            <Search />
-          </Button>
+    <div>
+      <div className="flex flex-col gap-6 items-center">
+        <div className="flex items-center justify-around w-full">
+          <h2 className="font-bold text-2xl hidden sm:block">Expense</h2>
 
-          <BalanceSheet />
+          <div className="flex gap-3">
+            <Button
+              className="md:hidden"
+              size="icon"
+              variant="primary"
+              onClick={() => setIsFormOpen(true)}
+            >
+              <CirclePlus />
+            </Button>
+
+            <Button
+              size="icon"
+              variant="outline"
+              onClick={() => setIsSearchOpen(true)}
+            >
+              <Search />
+            </Button>
+
+            <Button size="icon" variant="outline" onClick={scrollToPayments}>
+              <BanknoteArrowDown />
+            </Button>
+
+            <BalanceSheet />
+          </div>
         </div>
+
+        <ExpenseContainer participants={participants} />
       </div>
 
-      {/* Form Modal */}
-      {isFormOpen && (
-        <Suspense fallback={<Spinner />}>
-          <FormWrapper onClose={() => setIsFormOpen(false)}>
-            <ExpenseForm
-              onClose={() => setIsFormOpen(false)}
-              participants={participants}
-              onSubmit={() => setIsFormOpen(false)}
-            />
-          </FormWrapper>
-        </Suspense>
-      )}
-      {isSearchOpen && (
-        <SearchOverlay roomId={roomId} onClose={() => setIsSearchOpen(false)} />
-      )}
-
-      {/* Expense and Pending cards */}
-
-      <ExpenseContainer participants={participants} />
+      <div ref={paymentRef}>
+        <PaymentDetails participants={participants} userId={userId} />
+      </div>
     </div>
-    <PaymentDetails participants={participants} userId={userId}/>
-    </div>
-    
   );
 };
 
