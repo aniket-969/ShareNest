32c8461c5efe8542c2c9470478d2efa952634e93
Aniket <aniketbaranwal8090@gmail.com>
2026-03-26 18:29:04 +0530

    Updated authLayout for rerouting on basis of localStorage session

--- client/src/layouts/AuthLayout.jsx
+++ client/src/layouts/AuthLayout.jsx
@@ -5,12 +5,10 @@ import { Navigate, Outlet, useNavigate, Link } from "react-router-dom";
 import { useAuth } from "@/hooks/useAuth";
 
 const AuthLayout = () => {
-  const { sessionQuery } = useAuth();
-  const { data, isLoading } = sessionQuery;
+  const session = localStorage.getItem("session");
+  console.log(session);
 
-  // const session = localStorage.getItem("session");
-  // console.log(session);
-  return data ? (
+  return session ? (
     <Navigate to="/room" />
   ) : (
     <div className="flex items-center flex-col gap-6 max-h-screen mt-10 px-10">
