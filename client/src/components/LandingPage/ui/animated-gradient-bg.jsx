3e1b888340c28ad8e84506f077af3a727358e30f
Aniket <aniketbaranwal8090@gmail.com>
2026-04-03 19:02:14 +0530

    Landing page subtle animation

--- client/src/components/LandingPage/ui/animated-gradient-bg.jsx
+++ client/src/components/LandingPage/ui/animated-gradient-bg.jsx
@@ -8,7 +8,12 @@ export const AnimatedGradientBg = ({ className }) => {
 
       <div className="absolute top-0 left-0 w-full h-full">
         {/* Top-left glow */}
-        <div className="absolute top-1/4 left-1/4 w-96 h-96 bg-gradient-to-r from-pink-500/30 to-purple-500/30 rounded-full blur-3xl animate-pulse" />
+        <div
+          className="absolute top-1/4 left-1/4 w-96 h-96 
+bg-gradient-to-r from-pink-500/30 to-purple-500/30 
+rounded-full blur-3xl 
+animate-[glow_8s_ease-in-out_infinite]"
+        />
 
         {/* Bottom-right glow */}
         {/* <div className="absolute top-3/4 right-1/4 w-80 h-80 bg-gradient-to-r from-blue-500/30 to-cyan-500/30 rounded-full blur-3xl animate-pulse delay-1000" /> */}
@@ -16,10 +21,11 @@ export const AnimatedGradientBg = ({ className }) => {
         {/* Center glow */}
         <div
           className="absolute top-1/2 left-[60%] 
-                        transform -translate-x-1/2 -translate-y-1/2 
-                        w-[500px] h-[300px] 
-                        bg-gradient-to-r from-red-500/30 to-pink-500/30 
-                        rounded-full blur-3xl animate-pulse delay-2000"
+  -translate-x-1/2 -translate-y-1/2 
+  w-[500px] h-[300px] 
+  bg-gradient-to-r from-red-500/30 to-pink-500/30 
+  rounded-full blur-3xl 
+  animate-[glow_10s_ease-in-out_infinite]"
         />
       </div>
     </div>
